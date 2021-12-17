7c8ddb421feeb7a64b4735eaa8242a750d02df56
Masha1986 <9312613800@mail.ru>
2021-12-17 10:47:45 +0300

    Inicial commit

---
+++ src/Miles.java
@@ -0,0 +1,9 @@
+public class Miles {
+    public static void main(String[] args) {
+        int ticketPrice;
+        ticketPrice = 45;
+        int bonusmMiles = ticketPrice / 20;
+        System.out.println(bonusmMiles);
+
+    }
+}
